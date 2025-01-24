d05a1a7452f429e2343b3b98468155bc57d3b54d
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-24 15:24:31 +0100

    logic js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,21 @@
 // Milestone 3
 // Inseriamo il JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+
+//seleziono gli elementi di ouput
+
+//creo una costante e setto i parametri dell'endpoint
+
+//faccio partire la richiesta con axios per ricere le immagini relattive
+
+//in caso di successo
+
+    //creo un ciclo sull'array ricevuta per estrapolare i suoi oggetti
+
+    //creo una variabile e valorizzo il ciclo
+
+    //destrutturo l'ogetto
+
+    //utilizzo il template literal per creare il template e copio il codice html per stamparlo in pagina
+
+    //altrimenti manda messaggio di errore
+
